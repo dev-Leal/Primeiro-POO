ffd6433cbd0d57bf52d52c9febc30e52be883f7e
Beatriz <beatriz.leal343@gmail.com>
2020-05-26 20:12:33 -0300

    Primeiro POO

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace POO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            carro objeto = new carro ();
+            objeto.cor = "Preto";
+            objeto.modelo = "Mustang";
+            objeto.velocidade = 200; 
+            objeto.Andar ();
+
+            Console.WriteLine( objeto.cor);
+            Console.WriteLine( objeto.modelo);
+            Console.WriteLine( objeto.velocidade);
+            Console.WriteLine(objeto.Andar());
+        }    
+    }   
+    
+}
---
+++ carro.cs
@@ -0,0 +1,18 @@
+namespace POO
+{
+    public class carro
+    {
+        public string modelo;
+
+        public string cor;
+
+        public int velocidade;
+
+        public string Andar(){
+            return "O carro esta andando a " + velocidade ;
+        }
+
+
+
+    }
+}
